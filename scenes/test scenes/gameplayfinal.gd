9736a3735e63d9bbe019b887d80a3f4dabae0a1a
pawarherschel <pawarherschel@gmail.com>
2023-07-09 18:04:24 +0530

    gameplay

---
+++ scenes/test scenes/gameplayfinal.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+
+onready var invaders = $Invaders
+onready var bullets = $Bullets
+#
+#onready var score: PackedScene = preload("res://scenes/components/Score.tscn")
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GlobalState.reset()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Hero_hero_shot():
+	update_score()
+	pass # Replace with function body.
+
+func update_score():
+	GlobalState.add_score(invaders.available_children.size())
+
+
+func _on_Invaders_game_over():
+	update_score()
+	bullets.queue_free()
+	invaders.queue_free()
+	
+	Game.change_scene("res://scenes/components/Score.tscn")
+	
+	pass # Replace with function body.
